bf9cf53dacd5a9920d08821517be73d8f31b5510
Samuel Gómez <samusmusic18@gmail.com>
2025-05-29 20:25:39 -0500

    Agrega ejercicio 1 de estructuras selectivas

---
+++ Ejercicio1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+	double C1,C2,C3,P;
+	cout<<Digite la primera calificacion del alumno<<endl;
+	cin>>C1;
+	cout<<Digite la segunda calificacion del alumno<<endl;
+	cin>>C2;
+	cout<<Digite la tercera calificacion del alumno<<endl;
+	cin>>C3;
+	P=(C1+C2+C3)/3;
+	if(P>=70){
+		cout<<El alumno aprueba con un promedio de: <<P;
+	} else{
+		cout<<El alumno reprueba con un promedio de: <<P;
+	}
+	
+	
+	return 0;
+}
