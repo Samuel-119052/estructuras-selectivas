01845ff2abebaccc2679bc88f25368d6e6c7aeb6
Samuel Gómez <samusmusic18@gmail.com>
2025-05-31 08:54:43 -0500

    Mejora de validadcion de año en el ejercicio 10

--- Ejercicio10.cpp
+++ Ejercicio10.cpp
@@ -4,20 +4,24 @@ using namespace std;
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
 	int A;
-	cout<<Ingrese un año<<endl;
+	cout<<"Ingrese un año"<<endl;
 	cin>>A;
-	if(A%4==0){
-		if(A%100==0){
-			if(A%400==0){
-				cout<<Es un año bisiesto<<endl;
-			}else{
-				cout<<No es un año bisiesto<<endl;
-			}
-		}else{
-			cout<<Es un año bisiesto<<endl;
-		}
+	if(A>=0){
+		if(A%4==0){
+		    if(A%100==0){
+			    if(A%400==0){
+				    cout<<"Es un año bisiesto"<<endl;
+			    }else{
+				    cout<<"No es un año bisiesto"<<endl;
+			    }
+		    }else{
+			    cout<<"Es un año bisiesto"<<endl;
+		    }
+	    }else{
+		    cout<<"No es un año bisiesto"<<endl;
+	    }
 	}else{
-		cout<<No es un año bisiesto<<endl;
+		cout<<"Error: Numero negativo detectado"<<endl;
 	}
 	return 0;
 }
