52ce99ebd7f6d2fcb9999662544efd3852390a9b
Samuel Gómez <samusmusic18@gmail.com>
2025-05-31 08:32:29 -0500

    Agrega el ejercicio 10 de estructuras selectivas

---
+++ Ejercicio10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+	SetConsoleOutputCP(CP_UTF8);
+	int A;
+	cout<<Ingrese un año<<endl;
+	cin>>A;
+	if(A%4==0){
+		if(A%100==0){
+			if(A%400==0){
+				cout<<Es un año bisiesto<<endl;
+			}else{
+				cout<<No es un año bisiesto<<endl;
+			}
+		}else{
+			cout<<Es un año bisiesto<<endl;
+		}
+	}else{
+		cout<<No es un año bisiesto<<endl;
+	}
+	return 0;
+}
