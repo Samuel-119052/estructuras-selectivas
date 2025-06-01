bfdc4520bf0e16c50be8cd6efe884192e8500c8a
Samuel Gómez <samusmusic18@gmail.com>
2025-06-01 08:21:13 -0500

    Agregando el ejercicio 6 de estructuras selectivas

---
+++ Ejercicio6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double P,Pe,IGV,D,Pa;
+	cout<<Digite al pension educativa<<endl;
+	cin>>Pe;
+	cout<<Digite el promedio del alumno<<endl;
+	cin>>P;
+	if(P>=18){
+		D=Pe*0.30;
+	}else{
+		IGV=Pe*0.18;
+	}
+	Pa=(Pe-D)+IGV;
+	cout<<El alumno debera pagar el total de: <<Pa;
+	
+	return 0;
+}
