ea063c690c951710e9b89ea899258e11a44bbc0d
Samuel Gómez <samusmusic18@gmail.com>
2025-06-01 08:34:52 -0500

    Mejora del interfaz en el ejercicio 6 de estructuras selectivas

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main(){
 	double P,Pe,IGV,D,Pa;
-	cout<<Digite al pension educativa<<endl;
+	cout<<"PROGRAMA INICIADO"<<endl;
+	cout<<"*****************"<<endl;
+	cout<<"Digite al pension educativa"<<endl;
 	cin>>Pe;
-	cout<<Digite el promedio del alumno<<endl;
+	cout<<"Digite el promedio del alumno"<<endl;
 	cin>>P;
 	if(P>=18){
 		D=Pe*0.30;
@@ -12,7 +14,8 @@ int main(){
 		IGV=Pe*0.18;
 	}
 	Pa=(Pe-D)+IGV;
-	cout<<El alumno debera pagar el total de: <<Pa;
-	
+	cout<<"El alumno debera pagar el total de: "<<Pa<<endl;
+	cout<<"___________________"<<endl;
+	cout<<"PROGRAMA FINALIZADO"<<endl;
 	return 0;
 }
