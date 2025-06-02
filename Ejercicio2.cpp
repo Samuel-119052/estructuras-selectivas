9f3a915d5fab12c67456ba7af391d74527428dd4
Samuel Gómez <samusmusic18@gmail.com>
2025-06-02 09:21:56 -0500

    Agregar ejercicio 2 de estructuras selectivas

---
+++ Ejercicio2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double Ca, D, Tp;
+	cout<<Ingrese la cantidad de dinero que gasto en productos<<endl;
+	cin>>Ca;
+	if(Ca>1000){
+		D=Ca*0.20;
+	}else{
+		D=0;
+	}
+	Tp=Ca-D;
+	cout<<El total a pagar es: <<Tp;
+	return 0;
+}
